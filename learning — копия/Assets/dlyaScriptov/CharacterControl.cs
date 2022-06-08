bfa3cf367daaf299c6e867ee1d98ac79747db6fc
DanielPark5907 <105225797+DanielPark5907@users.noreply.github.com>
2022-06-08 18:09:50 +0600

    v1.4.1 main
    fixed bug with passing through walls

---
+++ learning — копия/Assets/dlyaScriptov/CharacterControl.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterControl : MonoBehaviour {
+	public float speed;
+	public float jSpeed;
+	public float gravity;
+	public float cofactor;
+
+	//приватные переменные
+	private Vector3 moveDir;
+	private CharacterController controller;
+
+	void Start () {
+		moveDir = Vector3.zero;
+		controller = GetComponent<CharacterController> ();
+	}
+	
+	void FixedUpdate () {
+		if (controller.isGrounded) {
+			moveDir = new Vector3 (Input.GetAxis ("Horizontal"), 0, Input.GetAxis ("Vertical"));
+			moveDir = transform.TransformDirection (moveDir);
+			moveDir *= speed*cofactor;
+		}
+		if (Input.GetKeyDown (KeyCode.Space)&& controller.isGrounded) {
+			moveDir.y = jSpeed;
+		}
+		if (Input.GetKey (KeyCode.LeftShift))
+			cofactor = 2f;
+		else if (Input.GetKeyUp (KeyCode.LeftShift))
+			cofactor = 1f;
+		else if (Input.GetKey (KeyCode.C))
+			cofactor = 0.5f;
+		else if (Input.GetKeyUp (KeyCode.C))
+			cofactor = 1f;
+		
+		moveDir.y -= gravity * Time.deltaTime;
+
+		controller.Move (moveDir * Time.deltaTime);
+	}
+}
