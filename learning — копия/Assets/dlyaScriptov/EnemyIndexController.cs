14df506222c44a8e51981cc5f1d7ab6ed3374000
DanielPark5907 <105225797+DanielPark5907@users.noreply.github.com>
2022-06-10 15:28:59 +0600

    added scripts for v2.1 main
    added 2 scripts:
    Spawner Script
    Enemy Index Controller script

---
+++ learning — копия/Assets/dlyaScriptov/EnemyIndexController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyIndexController : MonoBehaviour {
+	private WaveSpawner _waveSpawner;
+
+	void Start () {
+		_waveSpawner = GameObject.FindGameObjectWithTag ("WaveControll").GetComponent<WaveSpawner> ();
+		Debug.Log (_waveSpawner.gameObject.name);
+	}
+
+	void OnDestroy(){
+		int enemiesLeft;
+		enemiesLeft = GameObject.FindGameObjectsWithTag ("Zombi").Length;
+		Debug.Log (enemiesLeft);
+		if (enemiesLeft == 0) {
+			_waveSpawner.LaunchWave ();
+			Debug.Log ("New wave!");
+		}
+	}
+}
