8b482308e141bffb40fc8c398b846815cb1a6be2
DanielPark5907 <105225797+DanielPark5907@users.noreply.github.com>
2022-06-11 18:22:10 +0600

    Update Script

--- learning — копия/Assets/dlyaScriptov/EnemyIndexController.cs
+++ learning — копия/Assets/dlyaScriptov/EnemyIndexController.cs
@@ -1,13 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyIndexController : MonoBehaviour {
 	private WaveSpawner _waveSpawner;
+	private int waveNum = 1;
+	public Text wave;
+	private GameObject waveObj;
 
 	void Start () {
 		_waveSpawner = GameObject.FindGameObjectWithTag ("WaveControll").GetComponent<WaveSpawner> ();
 		Debug.Log (_waveSpawner.gameObject.name);
+		if (wave == null)
+			waveObj = GameObject.FindGameObjectWithTag ("WaveText");
+			wave = waveObj.GetComponent<Text> ();;
 	}
 
 	void OnDestroy(){
@@ -15,7 +22,9 @@ public class EnemyIndexController : MonoBehaviour {
 		enemiesLeft = GameObject.FindGameObjectsWithTag ("Zombi").Length;
 		Debug.Log (enemiesLeft);
 		if (enemiesLeft == 0) {
+			waveNum ++;
 			_waveSpawner.LaunchWave ();
+			wave.text = "Wave:" + waveNum;
 			Debug.Log ("New wave!");
 		}
 	}
