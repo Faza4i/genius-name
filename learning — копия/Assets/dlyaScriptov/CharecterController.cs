09cde13f5d6c32c1ebb397a14a2e625a98dee58d
DanielPark5907 <105225797+DanielPark5907@users.noreply.github.com>
2022-06-12 12:34:35 +0600

    Walk/Run/Sit sound

--- learning — копия/Assets/dlyaScriptov/CharecterController.cs
+++ learning — копия/Assets/dlyaScriptov/CharecterController.cs
@@ -2,48 +2,54 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharecterController : MonoBehaviour
-{
-    private Rigidbody rb;
-    public float Hspeed;
-    public float Vspeed;
-    public float jumpforse;
-    public float Cofactor = 1;
-    public bool isGrounded;
-    public Vector3 ToSpawn;
-    void Start()
-    {
-        rb = GetComponent <Rigidbody> ();
-    }
+public class CharacterControl : MonoBehaviour {
+	public float speed;
+	public float jSpeed;
+	public float gravity;
+	public float cofactor;
+	private AudioSource Au;
+	public AudioClip walkAu;
 
-    void FixedUpdate()
-    {
-        rb.velocity = new Vector3 (Input.GetAxis("Vertical")*-Vspeed*Cofactor*Time.fixedDeltaTime, rb.velocity.y , Input.GetAxis("Horizontal")*Hspeed*Cofactor*Time.fixedDeltaTime);
-    }
-    void Update(){
-        if(Input.GetKeyDown(KeyCode.Space)&& isGrounded){
-            rb.AddForce(0,jumpforse, 0);
-            isGrounded = false;
-        }
-        if(Input.GetKey(KeyCode.LeftShift))
-            Cofactor = 2;
-        else if(Input.GetKeyUp(KeyCode.LeftShift))
-            Cofactor = 1;
+	//приватные переменные
+	private Vector3 moveDir;
+	private CharacterController controller;
 
-        if(Input.GetKey(KeyCode.C))
-            Cofactor = 0.5f;
-        else if(Input.GetKeyUp(KeyCode.C))
-            Cofactor = 1;
+	void Start () {
+		moveDir = Vector3.zero;
+		controller = GetComponent<CharacterController> ();
+		Au = GetComponent<AudioSource> ();
+	}
+	
+	void FixedUpdate () {
+		if (controller.isGrounded) {
+			moveDir = new Vector3 (Input.GetAxis ("Horizontal"), 0, Input.GetAxis ("Vertical"));
+			moveDir = transform.TransformDirection (moveDir);
+			moveDir *= speed*cofactor;
+		}
+		if (moveDir != Vector3.zero) {
+			if (!Au.isPlaying)
+				Au.PlayOneShot (walkAu);
+		}
 
-        if(this.gameObject.GetComponent<Transform>().position.y <= -30)
-            this.gameObject.GetComponent<Transform>().position = ToSpawn;
-    }
-    void OnCollisionStay(Collision other){
-        if(other.gameObject.tag == "Ground")
-            isGrounded = true;
-    }
-    void OnCollisionExit(Collision other){
-        if (other.gameObject.tag == "Ground")
-            isGrounded = false;
-    }
+		if (Input.GetKeyDown (KeyCode.Space)&& controller.isGrounded) {
+			moveDir.y = jSpeed;
+		}
+		if (Input.GetKey (KeyCode.LeftShift)) {
+			cofactor = 2f;
+			Au.pitch = 2f;
+		} else if (Input.GetKeyUp (KeyCode.LeftShift)) {
+			cofactor = 1f;
+			Au.pitch = 1f;
+		} else if (Input.GetKey (KeyCode.C)) {
+			cofactor = 0.5f;
+			Au.pitch = 0.6f;
+		} else if (Input.GetKeyUp (KeyCode.C)) {
+			cofactor = 1f;
+			Au.pitch = 1f;
+		}
+		
+		moveDir.y -= gravity * Time.deltaTime;
+
+		controller.Move (moveDir * Time.deltaTime);
+	}
 }
