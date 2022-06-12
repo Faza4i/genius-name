631e24490f8c8ce451c9b9a3b33ffdd6aa6b6228
DanielPark5907 <105225797+DanielPark5907@users.noreply.github.com>
2022-06-12 18:07:35 +0600

    scripts for 2.5
    Update weapon system

--- learning — копия/Assets/dlyaScriptov/Weapon.cs
+++ learning — копия/Assets/dlyaScriptov/Weapon.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class Weapon : MonoBehaviour {
+	//Кол-во патрон в обойме
+	public int bulletsChamber;
+	//Макс кол-во в обойме(при перезарядке будет это кол-во)
+	public int bulletsMax;
+	//время перезаряки обоймы
+	public float seconds;
 	//сила отталкивания
 	public float force;
 	//урон
@@ -18,7 +24,9 @@ public class Weapon : MonoBehaviour {
 	public Transform bulletSpawn;
 	//звук стрельбы
 	public AudioClip shotFSX;
-	//где будет звук стрельбы
+	//звук перезарядки
+	public AudioClip RechargeSound;
+	//где будет звук оружия
 	public AudioSource AS;
 	//камера
 	public Camera _cam;
@@ -32,13 +40,15 @@ public class Weapon : MonoBehaviour {
 	private float nextFire = 0f;
 
 	void Start () {
-		
 	}
 	
 	void Update () {
-		if (Input.GetButton ("Fire1") && Time.time > nextFire) {
-			nextFire = Time.time + 1f / fireRate;
-			Shoot ();
+		if (bulletsChamber > 0) {
+			if (Input.GetButton ("Fire1") && Time.time > nextFire) {
+				bulletsChamber--;
+				nextFire = Time.time + 1f / fireRate;
+				Shoot ();
+			}
 		}
 	}
 
@@ -62,8 +72,9 @@ public class Weapon : MonoBehaviour {
 				GameObject blood = Instantiate (damageEffect, hit.point, Quaternion.LookRotation (hit.normal));
 				Destroy (blood, 2f);
 				hit.transform.gameObject.GetComponent<ZombiHP> ().HP -= damage;
-				ZombieHp.text = "ZoZo-HP: " + hit.transform.gameObject.GetComponent<ZombiHP> ().HP;
+				ZombieHp.text = "HpZombie" + hit.transform.gameObject.GetComponent<ZombiHP> ().HP;
 			}
 		}
 	}
+
 }
